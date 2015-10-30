7e1c9a893b087d7cef0b6b788f50d18402d25569
Lethe <alexandre.frizac@epitech.eu>
2015-10-30 15:25:51 +0100

    Conf du port série = magie noire

--- KFS/src/serial.c
+++ KFS/src/serial.c
@@ -5,16 +5,26 @@ void	serial_init()
 {
   // Désactive les interruptions
   outb(UART_ITR_ENABLE, 0x00);
+  // Mode fifo
+  outb(UART_FIFO_CONTROL, 0xC7);
 
+  // Vitesse
   outb(UART_LINE_CONTROL, 0x80);
-  outb(UART_BUFFER, 0x03);
-  outb(UART_DL_LS, 0x00);
-  outb(UART_LINE_CONTROL, 0x03);
+  outb(UART_DL_LS, 0x03);
+  outb(UART_DL_MS, 0x0);
+  outb(UART_LINE_CONTROL, 0x0);
+
+  // Configuration taille, bits stop & parité
+  outb(UART_LINE_CONTROL, 0x03 | 0x0 << 2 | 0x0 << 3);
+
+  // Options du modem
+  outb(UART_MODEM_CONTROL, 0x3);
 }
 
 void	serial_putchar(char c)
 {
   while (UART_TRANSMITTER_HOLDING_EMPTY == 0);
+  while (UART_CLEAR_TO_SEND == 0);
   outb(UART_BUFFER, c);
 }
 
